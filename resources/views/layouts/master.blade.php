de5db3362d1393c9e3341d4a11f05d37e48bef33
mahfuzali <mahfuzrifat94@gmail.com>
2020-03-11 21:54:29 +0600

    csrf-token

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -5,7 +5,7 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <meta http-equiv="x-ua-compatible" content="ie=edge">
-  <meta name="csrf-token" content="{{ csrf_token() }}">
+  <meta name="csrf-token" content="{{ csrf_token(5) }}">
   <title>Starter</title>
  <link rel="stylesheet" type="text/css" href="/css/app.css">
 </head>
